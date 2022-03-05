05032edef2c08ae568e8a50458233e2baa6e2de7
skyoo1226 <you1226@naver.com>
2022-03-06 00:03:57 +0900

    첫번째 프래그먼트를 화면에(xml) 배치

--- app/src/main/java/com/skyoo/fragment_20220305_exam/fragments/FirstFragment.kt
+++ app/src/main/java/com/skyoo/fragment_20220305_exam/fragments/FirstFragment.kt
@@ -1,11 +1,13 @@
 package com.skyoo.fragment_20220305_exam.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.skyoo.fragment_20220305_exam.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -15,6 +17,14 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnLogInFragment.setOnClickListener {
+            Log.d("프래그먼트", "첫번째 프래그먼트에서 로그 찍기")
+        }
     }
 
 }
